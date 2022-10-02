6da2c993147446dbb3bc1790ac7eae6b232652a6
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-10-02 21:03:43 +0200

    Hello world controller and /hola endpoint

---
+++ ob-rest-data-jpa/src/main/java/com/example/obrestdatajpa/controller/BookController.java
@@ -0,0 +1,8 @@
+package com.example.obrestdatajpa.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+}
---
+++ ob-rest-data-jpa/src/main/java/com/example/obrestdatajpa/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.obrestdatajpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        return "<h1>Hola Mundo!!</h1>";
+    }
+
+}
