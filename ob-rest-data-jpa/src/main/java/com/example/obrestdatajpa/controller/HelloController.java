2d23f516375596f4fcf43479617add3dd026dd46
Bryan Cusme <bryan.cusme@devoteam.com>
2022-10-04 11:24:43 +0200

    return html code from the controller - bad practise

--- ob-rest-data-jpa/src/main/java/com/example/obrestdatajpa/controller/HelloController.java
+++ ob-rest-data-jpa/src/main/java/com/example/obrestdatajpa/controller/HelloController.java
@@ -10,5 +10,22 @@ public class HelloController {
     public String holaMundo(){
         return "<h1>Hola Mundo!!</h1>";
     }
-
+    @GetMapping("tailwind")
+    public String tailwind(){
+        return """
+                <!doctype html>
+                <html>
+                <head>
+                  <meta charset="UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <script src="https://cdn.tailwindcss.com"></script>
+                </head>
+                <body>
+                  <h1 class="text-3xl text-sky-500 font-bold underline ">
+                    Hello world from Java API!
+                  </h1>
+                </body>
+                </html>
+                """;
+    }
 }
