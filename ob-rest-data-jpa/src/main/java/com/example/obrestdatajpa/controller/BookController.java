9f8b0d3ebbad43259537b4a18a7fbc30747e03bb
Bryan Cusme <bryan.cusme@devoteam.com>
2022-10-04 18:23:04 +0200

    create method with POST

--- ob-rest-data-jpa/src/main/java/com/example/obrestdatajpa/controller/BookController.java
+++ ob-rest-data-jpa/src/main/java/com/example/obrestdatajpa/controller/BookController.java
@@ -2,10 +2,12 @@ package com.example.obrestdatajpa.controller;
 
 import com.example.obrestdatajpa.entities.Book;
 import com.example.obrestdatajpa.repository.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -31,11 +33,30 @@ public class BookController {
         // La librería jackson es la que hace la conversión de código Java a formato JSON
         // De esta manera a la hora de realizar una petición desde un punto externo, este recibe un JSON
     }
-    // Buscar un libro por el id
 
+    // Buscar un libro por el id
+    @GetMapping("/api/v1/books/{id}")
+    public ResponseEntity<Book> findById(@PathVariable Long id) {
+        Optional<Book> bookOpt = bookRepository.findById(id);
+        // opción 1
+//        if (bookOpt.isPresent())
+//            return ResponseEntity.ok(bookOpt.get());
+//        else return ResponseEntity.notFound().build();
+        // opcion 2
+
+        return bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+
+    }
 
     // Crear un nuevo libro en la BBDD
+    @PostMapping("/api/v1/books")
+    public Book create(@RequestBody Book book, @RequestBody StreamingHttpOutputMessage.Body body) {
+        // guardarLibroRecibidoPorParámetro en la BBDD
+        System.out.println(body);
+        return bookRepository.save(book);
 
+    }
 
     // Acctualizar un libro existentte en la BBDD
 
